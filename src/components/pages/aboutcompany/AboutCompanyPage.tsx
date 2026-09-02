de816d575aaf55a342bff380a51d2b25b27f9225
sacrod-in-a-tech <sarinabalami123@gmail.com>
2026-09-02 21:04:52 +0545

    update the import section for about overview page.

--- src/components/pages/aboutcompany/AboutCompanyPage.tsx
+++ src/components/pages/aboutcompany/AboutCompanyPage.tsx
@@ -1,9 +1,15 @@
-
+import About from '../../sections/about/About';
+import AboutOverview from '../../sections/overview/AboutOverview';
+import AllInOne from '../../sections/allinone/AllInOne';
 import BlogsPage from '../../pages/blogs/BlogsPage';
+
 
 export default function AboutCompanyPage() {
   return (
     <main>
+      <About />
+      <AboutOverview />
+      <AllInOne />
       <BlogsPage />
     </main>
   );
